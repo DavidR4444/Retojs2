fe931fd4866ed8be163ef17b7ea61217c0197a0a
DavidR4444 <davidrubiano63@gmail.com>
2021-03-17 18:58:44 -0500

    actualizacion y agrego de archivos js y css

---
+++ assets/js/funcionalidad.js
@@ -0,0 +1,29 @@
+
+let casillaEscribir = document.getElementById("casillaEscribir");
+
+//numeros del teclado
+
+let btnNum1 = document.getElementById("btnNum1");
+let btnNum2 = document.getElementById("btnNum2");
+let btnNum3 = document.getElementById("btnNum3");
+let btnNum4 = document.getElementById("btnNum4");
+let btnNum5 = document.getElementById("btnNum5");
+let btnNum6 = document.getElementById("btnNum6");
+let btnNum7 = document.getElementById("btnNum7");
+let btnNum8 = document.getElementById("btnNum8");
+let btnNum9 = document.getElementById("btnNum9");
+let btnNum0 = document.getElementById("btnNum0");
+
+
+//letras
+
+let btnQ = document.getElementById("btnQ");
+let btnW = document.getElementById("btnW");
+let btnE = document.getElementById("btnE");
+let btnR = document.getElementById("btnR");
+let btnT = document.getElementById("btnT");
+let btnY = document.getElementById("btnY");
+let btnU = document.getElementById("btnU");
+let btnI = document.getElementById("btnI");
+let btnO = document.getElementById("btnO");
+let btnP = document.getElementById("btnP");
